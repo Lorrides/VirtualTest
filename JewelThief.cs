b8c355fe7e48fd8e1e924c882909165430f17c31
Lorrides <tarhh@outlook.com>
2016-02-02 12:43:24 +0100

    Laget prosjekt og enkle klasser

---
+++ JewelThief.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualTester
+{
+    class JewelThief : Locksmith
+    {
+        private Jewels _stolenJewels = null;
+
+        public new void ReturnContents(Jewels safeContents, Owner owner)
+        {
+            _stolenJewels = safeContents;
+            Console.WriteLine("Im stealing the contents! " + _stolenJewels.Sparkle());
+        }
+
+
+    }
+}
---
+++ Owner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualTester
+{
+    class Owner
+    {
+
+        private Jewels returnedContents;
+
+        public void ReciveContents(Jewels safeContents)
+        {
+            returnedContents = safeContents;
+            Console.WriteLine("Thank you for returning my jewels! " + safeContents.Sparkle());
+        }
+    }
+}
